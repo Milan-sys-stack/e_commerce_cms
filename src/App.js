e786a537d577a3f2db6b4c211300105d0fc0a9ff
Milan Ghimire <milanghimire656@gmail.com>
2023-07-16 14:21:20 +1000

    fist commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { SignIn } from "./components/pages/signin-singup/SignIn";
+import { SignUp } from "./components/pages/signin-singup/SignUp";
+function App() {
+  return (
+    <div className="">
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
